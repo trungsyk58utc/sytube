b84c12a2fb40b16559786208ea0a7eb72ff04a35
trungsyk58utc <cayconlontonzz@gmail.com>
2022-10-09 14:34:27 +0700

    last update

--- src/features/HomePage/HomePage.tsx
+++ src/features/HomePage/HomePage.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./HomePage.css";
-import { errorSwal } from "../../shared/alert";
 
 const HomePage = () => {
   return (
     <div className="home container">
-      <button onClick={() => errorSwal()}>Clickđasadsdsa</button>
       <h4>
         Mọi dữ liệu được lấy từ Youtube. Do hạn chế về unit được gọi từ Youtube
         trong ngày (tối đa 10000 unit/1 ngày, mỗi lần load trang gọi 100-200
