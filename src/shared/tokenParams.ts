bffb32517d9d6e5ea557e3b49dc2bae757f6d9e1
trungsyk58utc <cayconlontonzz@gmail.com>
2022-10-10 23:06:20 +0700

    add token

--- src/shared/tokenParams.ts
+++ src/shared/tokenParams.ts
@@ -1,2 +1,4 @@
-export const accesstoken = "AIzaSyBHUK74bemCKbsNKrt6UKZ0mtNjTJwv83w";
+export const accesstoken = "AIzaSyD7DWi10WKHM8KBW6l3j2Mru0VkOhu86Kw";
 // export const params = { key: accesstoken };
+//Mr.TienPQ3 token AIzaSyD7DWi10WKHM8KBW6l3j2Mru0VkOhu86Kw
+//SyDT token AIzaSyBHUK74bemCKbsNKrt6UKZ0mtNjTJwv83w
